8605572eafa543b9fae1b7fc4bd00daf277af61a
Olugbemiga <oadefuye@gmail.com>
2024-06-24 18:44:45 +0100

    The Third Commit

--- s3bucket.tf
+++ s3bucket.tf
@@ -8,7 +8,7 @@ provider "aws" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "cloudboosta-lab5-bucket"  # Replace with your desired unique bucket name
-  acl    = "private"
+
 
   tags = {
     Name        = "MyBucket"
